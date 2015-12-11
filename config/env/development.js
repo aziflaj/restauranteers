daf125ccaddcc8c1da28b406b1dadfa5a3232e36
Aldo Ziflaj <aldoziflaj95@gmail.com>
2015-12-11 17:27:37 +0100

    using memorydb in dev mode and mongolab in prod mode

--- config/env/development.js
+++ config/env/development.js
@@ -18,7 +18,7 @@ module.exports = {
    ***************************************************************************/
 
   models: {
-    connection: 'mongoDbServer',
+    connection: 'localDiskDb',
     migrate: 'drop'
   }
 
