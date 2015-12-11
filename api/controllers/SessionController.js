ca33b4bd2345303a8fe83e0c95bfef89c19b23b1
Aldo Ziflaj <aldoziflaj95@gmail.com>
2015-12-11 23:56:36 +0100

    stores a registered user

--- api/controllers/SessionController.js
+++ api/controllers/SessionController.js
@@ -19,14 +19,35 @@ module.exports = {
 				return res.send(error);
 			} else {
 				console.log('User found');
+				req.session.user_id = user.id;
+				req.session.authenticated = true;
 				return res.send(user);
 			}
 		});
 	},
 
 	register: function(req, res) {
-		return res.send({
-			message: 'Register a new user'
+		if (req.body.password !== req.body.password_repeat) {
+			console.log('pwd: ' + req.body.password);
+			console.log('rpt_pwd: ' + req.body.password_repeat);
+			return res.send('Passwords don\'t match'); //TODO: add some flash
+		}
+
+		var newUser = {
+			first_name: req.body.first_name,
+			last_name: req.body.last_name,
+			email: req.body.email,
+			username: req.body.username,
+			password: req.body.password
+		};
+
+		User.create(newUser, function(err, success) {
+			if (err) {
+				console.log(err);
+				return res.send(err);
+			} else {
+				return res.send(success);
+			}
 		});
 	}
 };
